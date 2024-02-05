dda751755e6aa7bd4bab84b23b3aa78f02fe6818
Vinicius Jaeger <viniciusjaegerprogramador@gmail.com>
2024-02-05 01:00:00 -0300

    First angular project commit - Dealing with Template

---
+++ src/app/Container/container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-component',
+    templateUrl: './container.component.html',
+    styleUrls: ['./container.component.css']
+})
+
+export class ContainerComponent {
+
+}
---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: `<div>
+                <p>This website provide cookies to provide better user experience!</p>
+              </div>`,
+  styleUrl: './notification.component.css'
+})
+export class NotificationComponent {
+
+}
