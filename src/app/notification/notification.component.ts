ace83a18daaee2c298ef5de322bac3fe7dd68bbc
Vinicius Jaeger <viniciusjaegerprogramador@gmail.com>
2024-02-05 11:26:02 -0300

    First angular project commit - Style, bootstrap and Selector properties

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div>
+  template: `<div class="alert alert-danger">
                 <p>This website provide cookies to provide better user experience!</p>
               </div>`,
-  styleUrl: './notification.component.css'
+  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; background-color: #FAD7A0; text-align: center;}", "p{font-size: 14px;}"]
 })
 export class NotificationComponent {
 
