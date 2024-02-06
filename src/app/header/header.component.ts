055c6d15e3906eb155c7ff5a4b247078db40766c
Vinicius Jaeger <viniciusjaegerprogramador@gmail.com>
2024-02-06 00:32:03 -0300

    Angular project - data binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  slogan: string = 'Your one stop shop for everything!';
+
+  source: string = '/assets/shopping.png';
 }
